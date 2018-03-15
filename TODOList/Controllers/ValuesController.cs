55384829d2f31d58121b72796e868b6b0eea4eef
bledardakaj <bd35191@ubt-uni.net>
2018-03-15 19:58:16 +0100

    puna e sotit qysh jemi marr vesh dje

---
+++ TODOList/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TODOList.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value"+id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            return value;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
