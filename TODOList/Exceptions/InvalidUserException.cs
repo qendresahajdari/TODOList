2377a9a5b609be2f952e6620a9398e2469f260c9
qendresahajdari <36197104+qendresahajdari@users.noreply.github.com>
2018-03-22 16:55:30 +0100

    Eshte kriju edhe exception per userin

---
+++ TODOList/Exceptions/InvalidUserException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TODOList.Exceptions
+{
+    public class InvalidUserException : Exception
+    {
+        public InvalidUserException()
+        {
+
+        }
+
+        public InvalidUserException(string username)
+            : base(String.Format("User already exist with username: {0}", username))
+        {
+
+        }
+    }
+}
